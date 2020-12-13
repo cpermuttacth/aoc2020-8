08a45ea9adcaed53171aa11716ff0969267ccc58
DESKTOP-TSL905K\Teemu <teemu.syvaoja@gmail.com>
2020-12-13 11:31:05 +0200

    8-2

--- 8/main.py
+++ 8/main.py
@@ -1,3 +1,5 @@
+import copy
+
 with open("input.txt") as file:
     file = file.read().splitlines()
 
@@ -12,7 +14,8 @@ class Computer:
 
     def step(self):
         self.step_counter += 1
-        self.process_code()
+        if 0 <= self.index < len(self.data):
+            self.process_code()
         return {"input_row": self.input_row, "accumulator": self.accumulator,
                 "index": self.index, "steps": self.step_counter}
 
@@ -54,12 +57,35 @@ def first_answer(input_data):
         used_rows.append(output["index"])
         if len(used_rows) != len(set(used_rows)):
             break
+    del computer
     return output["accumulator"]
 
 
 def second_answer(input_data):
-    return None
+
+    results = []
+    for i, row in enumerate(input_data):
+        input_copy = copy.deepcopy(input_data)
+        # switch jmp to nop or nop to jmp
+        if "jmp" in row:
+            input_copy[i] = row.replace("jmp", "nop")
+        elif "nop" in row:
+            input_copy[i] = row.replace("nop", "jmp")
+        else:
+            continue
+        computer = Computer(input_copy)
+        used_rows = []
+        while True:
+            output = computer.step()
+            used_rows.append(output["index"])
+            if output["index"] >= len(input_copy):
+                results.append(output["accumulator"])
+                break
+            if len(used_rows) > len(input_copy):
+                break
+        del computer
+    return set(results)
 
 
 print(f'First answer: {first_answer(file)}')
-print(f'Second answer: {second_answer(file)}')
+print(f'Second answer: {second_answer(file)}')  # arvatut 7
