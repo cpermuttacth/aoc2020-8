fa3869647446109356765274e63c54a09e9b31cc
teemusy <teemu.syvaoja@gmail.com>
2020-12-14 16:22:00 +0200

    Day 10 part 2

--- 10/main.py
+++ 10/main.py
@@ -1,7 +1,7 @@
 import copy
 from functools import reduce
 
-with open("test.txt") as file:
+with open("input.txt") as file:
     file = file.read().splitlines()
     file = [int(i) for i in file]
 
@@ -18,9 +18,9 @@ def first_answer(input_data):
         if i >= len(input_data) - 1:
             break
         if abs(item - input_data[i + 1]) == 1:
-            data["ones"] = data["ones"] + 1
+            data["ones"] += 1
         elif abs(item - input_data[i + 1]) == 3:
-            data["threes"] = data["threes"] + 1
+            data["threes"] += 1
     return data["ones"] * data["threes"]
 
 
@@ -32,25 +32,24 @@ def second_answer(input_data):
     # add device adapter in
     input_data.append(max(input_data) + 3)
     possible_combinations = []
-    print(input_data)
+    # divide list into sublist when there's a gap of 3
     for i, item in enumerate(input_data):
-        combination = []
-        counter = 2
-        if i >= len(input_data) - 1:
-            break
-        if input_data[i + 1] == item + 1:
-            start = i
-            end = None
-            while input_data[i + counter] == item + counter:
-                end = i + counter
-                counter += 1
-            if end is not None:
-                combination.append(input_data[start:end + 1])
-                possible_combinations.append(combination[0])
-    print(possible_combinations)
-    possible_combinations = [4 if len(x) == 4 else 2 for x in possible_combinations]
-    return_value = reduce(lambda x, y: x * y, possible_combinations)
-    return return_value, possible_combinations
+        start = i
+        end = None
+        counter = 1
+        while i + counter <= len(input_data) - 1 \
+                and (input_data[i + counter] == item + counter or input_data[i + counter] == item + counter + 1):
+            end = i + counter
+            counter += 1
+        if end is not None:
+            possible_combinations.append(input_data[start:end + 1])
+            for j in range(start, end):
+                input_data[j] = 0
+    possible_combinations = list(filter(lambda x: len(x) > 2, possible_combinations))
+    # multiplier depends on the size of sublist
+    values = {3: 2, 4: 4, 5: 7}
+    possible_combinations = list(map(lambda x: values[len(x)], possible_combinations))
+    return reduce((lambda x, y: x * y), possible_combinations)
 
 
 print(f'First answer: {first_answer(copy.deepcopy(file))}')
